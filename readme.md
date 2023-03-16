39c4ea9b69f804bf79f391212556df747d4f452a
Michael Maquera <mike.maquera212@gmail.com>
2023-03-15 22:00:46 -0400

    add light system

---
+++ readme.md
@@ -0,0 +1,21 @@
+# Three.js Test Bench
+
+## Description
+- A three.js test bench for experimenting with three.js features
+    - includes a particle system with randomly moving particles
+    - different three.js light types    
+    - a simple three.js scene with a cube and a sphere with a ortho camera
+
+## Installation and Usage 
+
+```bash
+
+npm i . 
+npm run dev
+npm run build
+
+```
+
+
+## Code Snippets
+- The code snippets are in the `src` folder
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import { GUI } from 'dat.gui'
 
 const sizes = {
   width: window.innerWidth,
@@ -12,14 +13,17 @@ let app = {
   scene: null,
   renderer: null,
   camera: null,
-  controls:null
+  controls:null,
+  light:null,
 }
 
 // initialize the application and THREE.js scene renderer
 const init = () => {
-  app.renderer = new THREE.WebGLRenderer({alpha: true});
+  app.renderer = new THREE.WebGLRenderer();
   app.renderer.setSize ( window.innerWidth, window.innerHeight);
   app.renderer.setClearColor(0x00000, 1) //0x424242
+  app.renderer.shadowMap.enabled = true;
+  //renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
   document.body.appendChild(app.renderer.domElement);
 
@@ -27,34 +31,63 @@ const init = () => {
   //app.scene.background = new THREE.Color( 0x000000 );
 
   app.camera =  new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  app.camera.position.z = 3;
-  app.camera.position.y = 1;
-
+  //app.camera.position.z = 3;
+  //app.camera.position.y = 1;
+  app.camera.up.set(0, 0, 1)
+  app.camera.position.set(0, -2.8, 0.14);
   
   // controls 
   app.controls = new OrbitControls( app.camera, app.renderer.domElement );
-  app.controls.enableDamping = true;
+  //app.controls.enableDamping = true;
+  
+  //createAmbientLight(); // create an ambient light
+ // createSpotLight();
+  createDirectionalLight();
+
 
 };
+
+
+function createPlane(){
+  const planeGeometry = new THREE.PlaneGeometry( 10, 10, 32 );
+  const planeMaterial = new THREE.MeshBasicMaterial( {color: "grey", side: THREE.DoubleSide} );
+  const plane = new THREE.Mesh( planeGeometry, planeMaterial );
+  plane.receiveShadow = true;
+  plane.castShadow = false;
+  app.scene.add( plane );
+
+}
 
 
 const createBox = () => {
   
   const geometry = new THREE.BoxGeometry( 1, 1, 1 );
   const material = new THREE.MeshBasicMaterial( { color: 0x212529 } );
+  
   const cube = new THREE.Mesh( geometry, material );
+  cube.position.set(1,3,1);
+  cube.castShadow = true;
+  cube.receiveShadow = false;
+  
+  app.light.target = cube
+  
   app.scene.add( cube );
+  
+  
+
+
 }
 
+
 const updateCube = () => {
-  //cube.rotation.y += 0.005;
+  cube.rotation.y += 0.005;
   //cube.position.x += 0.005;
   
 }
 
 const particles = []
 const particleGoalMap = new Map();
-const PARTICLE_COUNT = 1;
+const PARTICLE_COUNT = 5;
 const PARTICLE_SPEED = 0.01;
 
 
@@ -115,7 +148,9 @@ function moveParticleToGoal(particle){
   console.log("Distance to goal: " + currentPos.distanceTo(goal));
 
   
-  if(!isParticleGoalReached(currentPos,goal)){
+  if(!isParticleGoalReached(currentPos, goal)){
+
+
 
     // update the particle's position by adding the vector to the current position
     //let step = PARTICLE_SPEED * delta;
@@ -197,33 +232,52 @@ function createCricle(){
 
 function createSpotLight(){
 
-  const spotLight = new THREE.SpotLight( 0xffffff );
-  spotLight.position.set( 10, 10, 10 );
+  app.light = new THREE.SpotLight( 0xffffff );
+  app.light.position.set( 1, 5, 1 );
 
-  spotLight.map = new THREE.TextureLoader().load();
+  //spotLight.map = new THREE.TextureLoader().load();
 
-  spotLight.castShadow = true;
+  app.light.castShadow = true;
 
-  spotLight.shadow.mapSize.width = 1024;
-  spotLight.shadow.mapSize.height = 1024;
+  //app.light.shadow.mapSize.width = 1024;
+  //app.light.shadow.mapSize.height = 1024;
 
-  spotLight.shadow.camera.near = 500;
-  spotLight.shadow.camera.far = 4000;
-  spotLight.shadow.camera.fov = 20;
+  app.light.shadow.camera.near = 50;
+  app.light.shadow.camera.far = 40;
+  app.light.shadow.camera.fov = 20;
 
-  const spotLightHelper = new THREE.SpotLightHelper( spotLight );
+  const spotLightHelper = new THREE.SpotLightHelper( app.light );
   
 
  // spotLight.target = app.scene;
   app.scene.add( spotLightHelper );
-  app.scene.add( spotLight );
   
 
 }
 
+const createAmbientLight = () => { 
+  app.light = new THREE.AmbientLight(0x404040,8); 
+  app.light.position.set(3,3,3);
+  app.scene.add(app.light);
+
+}
+
+
 function createDirectionalLight(){
-  const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-  app.scene.add( directionalLight );
+  app.light = new THREE.DirectionalLight( 0x404040 , 0.5 );
+  app.light.castShadow = true; // default false
+  
+
+  //Set up shadow properties for the light
+  //app.light.shadow.mapSize.width = 512; // default
+  //app.light.shadow.mapSize.height = 512; // default
+  //app.light.shadow.camera.near = 0.5; // default
+ // app.light.shadow.camera.far = 500; // default
+      
+  app.light.position.set(1,2.9,4.11);
+  app.scene.add( app.light );
+
+
 }
 
 let clock = new THREE.Clock();
@@ -238,6 +292,33 @@ window.addEventListener('resize', () =>{
   app.renderer.setSize( window.innerWidth, window.innerHeight );
 });
 
+function createDATGUI(){
+
+  var gui = new GUI();
+
+  // example rotation of a cube mesh
+
+  //const cubeFolder = gui.addFolder('Cube')
+  //cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+  //cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+  //cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+  //cubeFolder.open()
+
+ 
+  const lightFolder = gui.addFolder('Light');
+  lightFolder.add(app.light.position, 'x', -5, 10).listen();
+  lightFolder.add(app.light.position, 'y', -5, 10).listen();
+  lightFolder.add(app.light.position, 'z', -5, 10).listen();
+  lightFolder.open()
+  
+
+  const cameraFolder = gui.addFolder('Camera')
+  cameraFolder.add(app.camera.position, 'x', -5, 10).listen();
+  cameraFolder.add(app.camera.position, 'y', -5, 10).listen();
+  cameraFolder.add(app.camera.position, 'z', -5, 10).listen();
+  cameraFolder.open()
+}
+
 
 const update = () => {
   
@@ -248,6 +329,7 @@ const update = () => {
   if(delta > interval) {
     
     //updateParticles();
+    updateCube();
 
     app.controls.update();
     app.renderer.render(app.scene, app.camera);
@@ -256,18 +338,19 @@ const update = () => {
 };
 
 
-
+// main program entry point
 function main(){
-  init();
-  //createBox();
+  init(); // init the size
+  
   createParticle();
+  createPlane();
 
-  //createSpotLight();
-  createDirectionalLight();
-
+  createBox();
   //createCricle();
+  
   update();
-
+  createDATGUI();
 }
+
 
 main();
